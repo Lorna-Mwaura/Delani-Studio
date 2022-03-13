90f69b5896ee551432932991051badf586add148
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-03-14 01:04:44 +0300

    added css styles

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+$("#design1").click(function () {
+    $("#design2").toggle();
+    $("#design3").toggle();
+});
+
+$("dev1").click(function () {
+    $("#dev2").toogle();
+    $("#dev3").toogle();
+});
+
+});

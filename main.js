895235a56121a9ec498d45d83e12be1a2e9a21ff
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-03-14 13:13:51 +0300

    added hover effect

--- main.js
+++ main.js
@@ -11,6 +11,52 @@ $(".pdct1").click(function () {
     $(".pdct2").toggle();
     $(".pdct3").toggle();
 });
+$(".project").mouseover(function(){
+    $("#project2").show();
+  })
+  $(".project").mouseout(function(){
+    $("#project2").hide();
+  });
+  $(".ontario").mouseover(function(){
+    $("#ontario2").show();
+  })
+  $(".ontario").mouseout(function(){
+    $("#ontario2").hide();
+  });
+
+  $(".diamond").mouseover(function(){
+    $("#diamond2").show();
+  })
+  $(".diamond").mouseout(function(){
+    $("#diamond2").hide();
+  });
+
+  $(".carrey").mouseover(function(){
+    $("#carrey2").show();
+  })
+  $(".carrey").mouseout(function(){
+    $("#carrey2").hide();
+  });
+  $(".elera").mouseover(function(){
+    $("#elera2").show();
+  })
+  $(".elera").mouseout(function(){
+    $("#elera2").hide();
+  });
+  $(".selector").mouseover(function(){
+    $("#selector2").show();
+  })
+  $(".selector").mouseout(function(){
+    $("#selector2").hide();
+  });
+  $(".burned").mouseover(function(){
+    $("#burned2").show();
+  })
+  $(".burned").mouseout(function(){
+    $("#burned2").hide();
+  });
+
+
 });
 
 let form = document.querySelector("#form")

49cf30a5ce1e9633cc97b7b5e071d578dc8bf477
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-03-14 10:43:40 +0300

    st-style background images

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ $(".design1").click(function () {
     $(".design2").toggle();
     $(".design3").toggle();
 });
+$(".dev1").click(function () {
+    $(".dev2").toggle();
+    $(".dev3").toggle();
+});
+$(".pdct1").click(function () {
+    $(".pdct2").toggle();
+    $(".pdct3").toggle();
+});
 });
